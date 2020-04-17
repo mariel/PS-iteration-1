d3d65b004c0ee87b5c7a8ed921b765d18a00f859
Mariel <mariel.petra@yahoo.com>
2020-04-17 16:35:46 +0300

    Cele 3 programe pentru tema7
    In primul am incercat sa scriu cat mai lizibil. In cel de-al doilea am incercat sa il fac mai rapid, iar cel de-al treilea nu am stiut cum sa il fac extensibil(?).

---
+++ src/Counter3.java
@@ -0,0 +1,9 @@
+package demo;
+
+//I know what an extendable program means
+//I don't know how to write one :( 
+//Ma gandeam sa fac cazuri in functie de extensia fisierului, dar nu stiu cum sa fac sa mearga :(
+
+public class Counter3 {
+
+}
