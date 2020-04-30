25cc941fe6267771c252d3123fd65262d17d222f
Mariel <mariel.petra@yahoo.com>
2020-04-30 03:07:50 +0300

    Started to create the GUI
    I created an sketch of what the final product will most likely be able to do.
    The GUI here is most probably not the final one.
    I have the city map and the edit buttons; each button goes to a edit window. In the new edit windows I can Create Delete and Update; The Read I want it from the main GUI

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -1,20 +1,81 @@
 package view;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 public class Gui {
-	JFrame f = new JFrame();
-	JButton b = new JButton("Land");
+	private JFrame f = new JFrame();
+	private JButton[][] cityMap;
+	private Border border = new LineBorder(Color.LIGHT_GRAY,2,true);
+	
+	private JButton editHydro = new JButton("Edit Hydro");
+	private JButton editPlaces = new JButton("Edit Places");
+	private JButton editAmenities = new JButton("Edit Amenities");
 	
 	public Gui() {
-		b.setBounds(0, 0, 5, 5);
-		b.setBackground(Color.BLACK);
 		
-		f.add(b);
-		f.setSize(400,500);
+		cityMap = new JButton[80][100];
+		
+		for (int i = 0; i < 80; i++)
+		{
+			for (int j = 0; j < 100; j++)
+			{
+				cityMap[i][j] = new JButton("Land");
+			}
+		}
+
+		
+		for (int i = 0; i < 80; i++)
+		{
+			for(int j = 0; j < 100; j++)
+			{
+				cityMap[i][j].setBounds(i*10, j*10, 10, 10);
+				cityMap[i][j].setBackground(Color.LIGHT_GRAY);
+				cityMap[i][j].setBorder(border);
+				f.add(cityMap[i][j]);
+			}
+		}
+
+		
+		editHydro.setBounds(820, 50, 150, 50);
+		editPlaces.setBounds(820, 110, 150, 50);
+		editAmenities.setBounds(820, 170, 150, 50);
+		
+		f.add(editHydro);
+		f.add(editPlaces);
+		f.add(editAmenities);
+		
+		
+		editHydro.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				HydroGui hg = new HydroGui();				
+			}
+		});
+		
+		editPlaces.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				PlaceGui pg = new PlaceGui();				
+			}
+		});
+		
+		editAmenities.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				AmenityGui ag = new AmenityGui();				
+			}
+		});
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		f.setSize(1100,1100);
 		f.setLayout(null);
 		f.setVisible(true);
 	}
+	
+	
 }
---
+++ src/view/PlaceGui.java
@@ -0,0 +1,39 @@
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class PlaceGui {
+
+	private JFrame f = new JFrame();
+	private JButton add = new JButton("ADD");
+	private JButton delete = new JButton("DELETE");
+	private JButton update = new JButton ("UPDATE");
+	
+	private JTextField nameAdd = new JTextField();
+	private JComboBox nameDelete = new JComboBox();
+	private JComboBox nameUpdate = new JComboBox();
+
+	public PlaceGui() {
+		
+		add.setBounds(850, 100, 100, 50);
+		delete.setBounds(850, 250, 100, 50);
+		update.setBounds(850, 400, 100, 50);
+		nameAdd.setBounds(50, 100, 200, 25);
+		nameDelete.setBounds(50, 250, 200, 25);
+		nameUpdate.setBounds(50, 400, 200, 25);
+		
+		f.add(add);
+		f.add(delete);
+		f.add(update);
+		f.add(nameAdd);
+		f.add(nameDelete);
+		f.add(nameUpdate);
+
+		f.setSize(1000,500);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+}
